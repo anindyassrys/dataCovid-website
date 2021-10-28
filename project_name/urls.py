2d0b30b949381a2b386316674644e51037d676a4
Al Ghifari Enerza Sentanu <al.ghifari01@ui.ac.id>
2021-10-28 20:43:56 +0700

    commit untuk /rujukan
    menambahkan /rujukan pada settings.py dan urls.py

--- project_name/urls.py
+++ project_name/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+import rujukan.urls as rsrujukan
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('rumah-sakit-rujukan/', include(rsrujukan)),
 ]
