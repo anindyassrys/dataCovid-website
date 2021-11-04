b3cbd1afbc7d9cb2fc19e9d257665679a1226e92
Arkan_Fauzan <ozza203@gmail.com>
2021-11-05 00:40:47 +0700

    update page content

--- home/static/home.js
+++ home/static/home.js
@@ -1,27 +1,3 @@
-// // panggil function getGlobalInfo ketika halaman sudah berhasil di load
-// $(document).ready(function(){
-//     getIndonesiaInfo();
-// });
-
-// // membuat fungsi untuk mendapatkan jumlah di negara Indonesia
-// function getIndonesiaInfo(){
-//     $.ajax({
-//         url : 'https://coronavirus-19-api.herokuapp.com/countries/Indonesia',
-//         success: function (data) {
-
-//             try{
-//                 var json = data;
-//                 var positif = json.todayCases;
-
-//                 $("positif").html(positif);
-//             }catch(e){
-//                 alert("Gagal mendapatkan data dari server")
-//             }
-//         }, error : function(resp) {
-
-//         }
-//     });
-// }
 // fetch data from API
 fetch('https://corona.lmao.ninja/v2/countries/indonesia')
 .then((response)=>{
